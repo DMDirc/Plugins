ed363a206d28474ae921256c37434ff44fe2ddd0
Gregory Holmes <greg@dmdirc.com>
2010-01-05 20:49:06 +0000

    Fixes issue 3413: go upload a better placeholder addonbrowser
    Change-Id: I3c7575f90661c177bd5432cf0e961f1980980d3d

---
+++ src/com/dmdirc/addons/addonbrowser/BrowserPlugin.java
@@ -0,0 +1,54 @@
+/*
+ * 
+ * Copyright (c) 2006-2010 Chris Smith, Shane Mc Cormack, Gregory Holmes
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc.addons.addonbrowser;
+
+import com.dmdirc.config.prefs.PreferencesCategory;
+import com.dmdirc.config.prefs.PreferencesManager;
+import com.dmdirc.plugins.Plugin;
+
+/**
+ * Placeholder addon browser plugin.
+ */
+public class BrowserPlugin extends Plugin {
+
+    /** {@inheritDoc} */
+    @Override
+    public void onLoad() {
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void onUnload() {
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void showConfig(final PreferencesManager manager) {
+        PreferencesCategory category = new PreferencesCategory("Addon Browser",
+                "This addon is now part of the Swing UI and is no longer " +
+                "required. The plugin may be manually deleted from your " +
+                "plugins directory.");
+        manager.getCategory("Plugins").addSubCategory(category);
+    }
+}
