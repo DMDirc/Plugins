8b43b48763e50a7db45b77b4cf3a62c3d7cccddd
Gregory Holmes <greg@dmdirc.com>
2010-01-09 00:55:57 +0000

    Make about dialog big again
    Change-Id: Ie186542d72a3c987781b35f6787f5d9dccd51adc

--- src/com/dmdirc/addons/ui_swing/dialogs/about/AboutDialog.java
+++ src/com/dmdirc/addons/ui_swing/dialogs/about/AboutDialog.java
@@ -116,7 +116,8 @@ public final class AboutDialog extends StandardDialog implements
         tabbedPane.add("Information", new InfoPanel());
         tabbedPane.addChangeListener(this);
 
-        getContentPane().setLayout(new MigLayout("ins rel, wrap 1, fill, wmax 550, hmax 400"));
+        getContentPane().setLayout(new MigLayout("ins rel, wrap 1, fill, " +
+                "wmin 550, wmax 550, hmin 400, hmax 400"));
         getContentPane().add(tabbedPane, "grow, push");
         getContentPane().add(getOkButton(), "right");
     }
