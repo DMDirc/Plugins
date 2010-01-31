963c6c458b96cc5e20581315bd37d57d4865e4dd
Gregory Holmes <greg@dmdirc.com>
2010-01-31 04:26:10 +0000

    Adds DMDircHTMLEditor kit to TextLabel, this enables the use of DMDirc URLs in the HTML contained within these labels
    Change-Id: I9b183b113f740e4549239359b7070e487dfb0fe7
    Reviewed-on: http://gerrit.dmdirc.com/756
    Automatic-Compile: Chris Smith <chris@dmdirc.com>
    Reviewed-by: Chris Smith <chris@dmdirc.com>

--- src/com/dmdirc/addons/ui_swing/components/text/TextLabel.java
+++ src/com/dmdirc/addons/ui_swing/components/text/TextLabel.java
@@ -22,19 +22,28 @@
 
 package com.dmdirc.addons.ui_swing.components.text;
 
+import com.dmdirc.util.URLBuilder;
+
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Insets;
+import java.net.URL;
 
 import javax.swing.JTextPane;
 import javax.swing.UIManager;
 import javax.swing.plaf.basic.BasicTextPaneUI;
 import javax.swing.text.DefaultStyledDocument;
+import javax.swing.text.Element;
 import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
 import javax.swing.text.StyledDocument;
+import javax.swing.text.View;
+import javax.swing.text.ViewFactory;
+import javax.swing.text.html.HTML;
 import javax.swing.text.html.HTMLDocument;
 import javax.swing.text.html.HTMLEditorKit;
+import javax.swing.text.html.HTMLEditorKit.HTMLFactory;
+import javax.swing.text.html.ImageView;
 import javax.swing.text.html.StyleSheet;
 
 /**
@@ -75,18 +84,19 @@ public class TextLabel extends JTextPane {
      */
     public TextLabel(final String text, final boolean justified) {
         super(new DefaultStyledDocument());
-        setEditorKit(new HTMLEditorKit());
+        setEditorKit(new DMDircHTMLEditorKit());
         setUI(new BasicTextPaneUI());
 
-        final StyleSheet styleSheet = ((HTMLDocument) getDocument()).getStyleSheet();
+        final StyleSheet styleSheet = ((HTMLDocument) getDocument()).
+                getStyleSheet();
         final Font font = UIManager.getFont("Label.font");
         final Color colour = UIManager.getColor("Label.foreground");
-        styleSheet.addRule("body " 
+        styleSheet.addRule("body "
                 + "{ font-family: " + font.getFamily() + "; "
                 + "font-size: " + font.getSize() + "pt; }");
         styleSheet.addRule("p { margin: 0; }");
         styleSheet.addRule("* { color: rgb(" + colour.getRed()
-                + ", " + colour.getGreen() + ", " + colour.getBlue()+"); }");
+                + ", " + colour.getGreen() + ", " + colour.getBlue() + "); }");
 
         setOpaque(false);
         setEditable(false);
@@ -114,5 +124,64 @@ public class TextLabel extends JTextPane {
         if (t != null && !t.isEmpty()) {
             getDocument().setParagraphAttributes(0, t.length(), sas, false);
         }
+    }
+}
+
+/**
+ * DMDirc html kit, extends HTMLEditor kit to use DMDircHTMLFactory.
+ */
+class DMDircHTMLEditorKit extends HTMLEditorKit {
+
+    private static final long serialVersionUID = 1;
+    private ViewFactory defaultFactory = new DMDircHTMLFactory();
+
+    /** {@inheritDoc} */
+    @Override
+    public ViewFactory getViewFactory() {
+        return defaultFactory;
+    }
+}
+
+/**
+ * DMDircHTML factory extends HTMLFactory to use DMDircImageView.
+ */
+class DMDircHTMLFactory extends HTMLFactory {
+
+    /** {@inheritDoc} */
+    @Override
+    public View create(Element elem) {
+        final View view = super.create(elem);
+        if (view instanceof ImageView) {
+            return new DMDircImageView(elem);
+        }
+        return view;
+    }
+}
+
+/**
+ * DMDirc image view, extends default image view but uses DMDirc URLBuilder.
+ */
+class DMDircImageView extends ImageView {
+
+    /**
+     * Creates a new DMDirc image view.
+     *
+     * @param elem element to view
+     */
+    public DMDircImageView(final Element elem) {
+        super(elem);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @return URL to resource
+     */
+    @Override
+    public URL getImageURL() {
+        final String src = (String) getElement().getAttributes().
+                getAttribute(HTML.Attribute.SRC);
+
+        return src == null ? null : URLBuilder.buildURL(src);
     }
 }
