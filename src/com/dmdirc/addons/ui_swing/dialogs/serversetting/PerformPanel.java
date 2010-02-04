ee76331fe29d682a43a4e96e723ed1d7d6b58183
Gregory Holmes <greg@dmdirc.com>
2010-02-04 21:20:49 +0000

    Fixes issue 3733: SSD per profile performs dont work at all
    Change-Id: I899eea7dc76109d5c98b66570a79aaa2081c061b

--- src/com/dmdirc/addons/ui_swing/dialogs/serversetting/PerformPanel.java
+++ src/com/dmdirc/addons/ui_swing/dialogs/serversetting/PerformPanel.java
@@ -77,6 +77,9 @@ public final class PerformPanel extends JPanel implements ActionListener {
     /** Perform text area. */
     private JTextArea textarea;
 
+    /** Active perform. */
+    private int activePerform = 0;
+
     /**
      * Creates a new instance of IgnoreList.
      *
@@ -84,7 +87,6 @@ public final class PerformPanel extends JPanel implements ActionListener {
      */
     public PerformPanel(final Server server) {
         super();
-
         this.server = server;
 
         this.setOpaque(UIUtilities.getTabbedPaneOpaque());
@@ -123,6 +125,7 @@ public final class PerformPanel extends JPanel implements ActionListener {
         target.addActionListener(this);
     }
 
+
     /** Loads the perform actions. */
     private void loadPerforms() {
         serverAction = PerformWrapper.getPerformWrapper().
@@ -155,7 +158,7 @@ public final class PerformPanel extends JPanel implements ActionListener {
         if (profileNetworkAction == null) {
             profileNetworkPerform = "";
         } else {
-            profileNetworkPerform = implode(profileServerAction.getResponse());
+            profileNetworkPerform = implode(profileNetworkAction.getResponse());
         }
     }
 
@@ -180,15 +183,19 @@ public final class PerformPanel extends JPanel implements ActionListener {
     private void populatePerform() {
         switch (target.getSelectedIndex()) {
             case 0:
+                System.out.println("network perform: " + networkPerform);
                 textarea.setText(networkPerform);
                 break;
             case 1:
+                System.out.println("profile network perform: " + profileNetworkPerform);
                 textarea.setText(profileNetworkPerform);
                 break;
             case 2:
+                System.out.println("server perform: " + serverPerform);
                 textarea.setText(serverPerform);
                 break;
             case 3:
+                System.out.println("profile server perform: " + profileServerPerform);
                 textarea.setText(profileServerPerform);
                 break;
             default:
@@ -198,7 +205,7 @@ public final class PerformPanel extends JPanel implements ActionListener {
 
     /** Stores the text currently in the textarea into the perform strings. */
     private void storeText() {
-        switch (target.getSelectedIndex()) {
+        switch (activePerform) {
             case 0:
                 networkPerform = textarea.getText();
                 break;
@@ -220,7 +227,7 @@ public final class PerformPanel extends JPanel implements ActionListener {
     public void savePerforms() {
         storeText();
 
-        if (!serverPerform.isEmpty() || serverAction != null) {
+        if (!serverPerform.isEmpty()) {
             if (serverAction == null) {
                 serverAction = PerformWrapper.getPerformWrapper().
                         createActionForServer(server.getName());
@@ -229,7 +236,7 @@ public final class PerformPanel extends JPanel implements ActionListener {
             serverAction.save();
         }
 
-        if (!networkPerform.isEmpty() || networkAction != null) {
+        if (!networkPerform.isEmpty()) {
             if (networkAction == null) {
                 networkAction = PerformWrapper.getPerformWrapper().
                         createActionForNetwork(server.getNetwork());
@@ -238,7 +245,7 @@ public final class PerformPanel extends JPanel implements ActionListener {
             networkAction.save();
         }
 
-        if (!profileNetworkPerform.isEmpty() || profileNetworkAction != null) {
+        if (!profileNetworkPerform.isEmpty()) {
             if (profileNetworkAction == null) {
                 profileNetworkAction = PerformWrapper.getPerformWrapper().
                         createActionForNetwork(server.getNetwork(),
@@ -248,7 +255,7 @@ public final class PerformPanel extends JPanel implements ActionListener {
             profileNetworkAction.save();
         }
 
-        if (!profileServerPerform.isEmpty() || profileServerAction != null) {
+        if (!profileServerPerform.isEmpty()) {
             if (profileServerAction == null) {
                 profileServerAction = PerformWrapper.getPerformWrapper().
                         createActionForServer(server.getName(),
@@ -267,6 +274,7 @@ public final class PerformPanel extends JPanel implements ActionListener {
     @Override
     public void actionPerformed(final ActionEvent e) {
         storeText();
+        activePerform = target.getSelectedIndex();
         populatePerform();
     }
 
