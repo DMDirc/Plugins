eae0a6685cf1166519a0b0ae10d7d3bc2d83f6a5
Simon Mott <simon@dmdirc.com>
2010-03-28 02:28:55 +0100

    Mouse scrolling of buttonbar works the same as the treeview.
    issue 4062
    
    Change-Id: I70082e47c387462e656a566e72ee390cba331770

--- src/com/dmdirc/addons/ui_swing/framemanager/buttonbar/ButtonPanel.java
+++ src/com/dmdirc/addons/ui_swing/framemanager/buttonbar/ButtonPanel.java
@@ -22,9 +22,12 @@
  */
 package com.dmdirc.addons.ui_swing.framemanager.buttonbar;
 
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Rectangle;
 
+import java.awt.event.MouseWheelEvent;
+import java.awt.event.MouseWheelListener;
 import javax.swing.JPanel;
 import javax.swing.Scrollable;
 
@@ -36,7 +39,7 @@ import net.miginfocom.swing.MigLayout;
  * @author Simon Mott
  * @since 0.6.4
  */
-public class ButtonPanel extends JPanel implements Scrollable {
+public class ButtonPanel extends JPanel implements Scrollable, MouseWheelListener {
 
     /** The ButtonBar that created this Panel. */
     private ButtonBar buttonBar;
@@ -80,6 +83,54 @@ public class ButtonPanel extends JPanel implements Scrollable {
     @Override
     public boolean getScrollableTracksViewportHeight() {
         return false;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void mouseWheelMoved(MouseWheelEvent e) {
+        e.consume();
+        final int selectedIndex = getSelectedIndex();
+        int newIndex = 0;
+        if (e.getWheelRotation() < 0) {
+            //Up
+            //Check if selectedIndex is < 0 and if it is, get the index
+            //of the bottom most button, else select the next button
+            //up if buttonbar is vertical or left if horizontal
+            newIndex = selectedIndex - 1 >= 0 ? selectedIndex - 1 :
+                getComponentCount() - 1;
+        } else if (e.getWheelRotation() > 0) {
+            //Down
+            //Check if selectedIndex is greater than the total number of buttons,
+            //if it is select the first button in the list, else choose the next
+            //button down if buttonbar is vertial or right if horizontal
+            newIndex = (selectedIndex + 1) % getComponentCount();
+        }
+        final FrameToggleButton button = ((FrameToggleButton) getComponent(newIndex));
+        button.getWindow().activateFrame();
+    }
+
+    /**
+     * Gets the component index of the button associated with the current
+     * selected window.
+     *
+     * @return Integer Index for the button of the selected window
+     *
+     * @since 0.6.4
+     */
+    private int getSelectedIndex() {
+        FrameToggleButton button;
+        int i = 0;
+        int selectedIndex = 0;
+        final FrameToggleButton selectedButton = buttonBar.getSelectedButton();
+        for (Component c : getComponents()) {
+            button = (FrameToggleButton) c;
+            if (button == selectedButton) {
+                selectedIndex = i;
+                break;
+            }
+            i++;
+        }
+        return selectedIndex;
     }
 
 }
