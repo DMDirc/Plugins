9656945b4b76f26ed94a770a673c113007478e82
Chris Smith <chris@dmdirc.com>
2010-04-15 00:29:33 +0100

    Fix rogue ".".
    Fixes issue 3872
    
    Change-Id: I55159e41cb85759e2cd12bd352c6e81a6316145a
    Reviewed-on: http://gerrit.dmdirc.com/1259
    Reviewed-by: Gregory Holmes <greg@dmdirc.com>
    Automatic-Compile: DMDirc Local Commits <dmdirc@googlemail.com>

--- src/com/dmdirc/addons/parser_twitter/TwitterPlugin.java
+++ src/com/dmdirc/addons/parser_twitter/TwitterPlugin.java
@@ -95,7 +95,7 @@ public class TwitterPlugin extends Plugin  {
                 getPluginInfo(), "Advanced", "Advanced Settings related to the twitter plugin",
                 "category-twitter");
 
-        category.addSetting(new PreferencesSetting(PreferencesType.INTEGER, getDomain(), "statuscount", "Statuses to request", "How many statuses to request at a time.?"));
+        category.addSetting(new PreferencesSetting(PreferencesType.INTEGER, getDomain(), "statuscount", "Statuses to request", "How many statuses to request at a time?"));
         category.addSetting(new PreferencesSetting(PreferencesType.INTEGER, getDomain(), "apicalls", "API Calls", "Aim to only use how many API Calls per hour? (Twitter has a max of 150)"));
         category.addSetting(new PreferencesSetting(PreferencesType.BOOLEAN, getDomain(), "saveLastIDs", "Remember shown items", "Should previously shown items not be shown again next time?"));
         category.addSetting(new PreferencesSetting(PreferencesType.BOOLEAN, getDomain(), "getSentMessages", "Show own Direct Messages", "Should we try to show our own direct messages to people not just ones to us?"));
