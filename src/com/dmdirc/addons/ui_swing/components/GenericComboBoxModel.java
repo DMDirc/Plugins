54b8bacb9e6d85a0e58821e1f3a7b9cd7834d464
Greg Holmes <greg@dmdirc.com>
2010-05-23 02:43:23 +0100

    Add generic combo box model.
    Fixes issue 4230
    
    Change-Id: I30cf8d2b3cc7e67357dd42cf0b320e1097983fba

---
+++ src/com/dmdirc/addons/ui_swing/components/GenericComboBoxModel.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2006-2010 Chris Smith, Shane Mc Cormack, Gregory Holmes
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc.addons.ui_swing.components;
+
+import javax.swing.ComboBoxModel;
+
+/**
+ * Generic combo box model, extends generic list model.
+ *
+ * @param <T> Model holds this type of object
+ */
+public class GenericComboBoxModel<T> extends GenericListModel<T> implements
+        ComboBoxModel {
+
+    /**
+     * A version number for this class. It should be changed whenever the class
+     * structure is changed (or anything else that would prevent serialized
+     * objects being unserialized with the new class).
+     */
+    private static final long serialVersionUID = 2;
+    /** Selected Object. */
+    private T selectedObject;
+
+    /** {@inheritDoc} */
+    @Override
+    @SuppressWarnings("unchecked")
+    public void setSelectedItem(final Object anItem) {
+        if ((selectedObject != null && !selectedObject.equals(anItem)) || selectedObject
+                == null && anItem != null) {
+            selectedObject = (T) anItem;
+            fireContentsChanged(this, -1, -1);
+        }
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public T getSelectedItem() {
+        return selectedObject;
+    }
+}
