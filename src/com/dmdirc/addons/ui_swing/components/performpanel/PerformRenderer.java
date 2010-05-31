be2aa4fe4335a23fb4e9da2cac2851b79c9ad7ee
Simon Mott <simon@dmdirc.com>
2010-05-31 16:12:27 +0100

    Display server/network name or text in perform dropdown on the server settings dialog
    fixes issue 4243
    
    Change-Id: Ie50b567461464ee148e9694442f4002778b1f68a
    Reviewed-on: http://gerrit.dmdirc.com/1351
    Reviewed-by: Chris Smith <chris@dmdirc.com>
    Automatic-Compile: DMDirc Local Commits <dmdirc@googlemail.com>

--- src/com/dmdirc/addons/ui_swing/components/performpanel/PerformRenderer.java
+++ src/com/dmdirc/addons/ui_swing/components/performpanel/PerformRenderer.java
@@ -51,10 +51,11 @@ public class PerformRenderer extends DefaultListCellRenderer {
         String friendlyText = type + " perform (" + target + ") ";
 
         if (profile != null) {
-            friendlyText = "This profile (" + profile + ")";
+            friendlyText += "This profile (" + profile + ")";
         } else {
-            friendlyText = "Any profile";
+            friendlyText += "Any profile";
         }
+
         return super.getListCellRendererComponent(list, friendlyText, index,
                 isSelected, cellHasFocus);
     }
