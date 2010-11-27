456bb3eec2684176ea80d017a0ad5c9f1fa3772b
Simon Mott <simon@dmdirc.com>
2010-11-27 21:09:55 +0000

    Adds a Timer Manager that allows cancelling of timers
    Fixes issue CLIENT-101
    
    Change-Id: I47c6b053fe451cfd6f734959195926bad7fc6376

--- src/com/dmdirc/addons/time/TimerCommand.java
+++ src/com/dmdirc/addons/time/TimerCommand.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2006-2010 Chris Smith, Shane Mc Cormack, Gregory Holmes
+ * Copyright (c) 2006-2010 Chris Smith, Shane Mc Cormack, Gregory Holmes,
+ * Simon Mott
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -30,7 +31,8 @@ import com.dmdirc.commandparser.commands.Command;
 import com.dmdirc.commandparser.commands.IntelligentCommand;
 import com.dmdirc.commandparser.commands.context.CommandContext;
 import com.dmdirc.ui.input.AdditionalTabTargets;
-import com.dmdirc.ui.input.TabCompleter;
+import java.util.Map.Entry;
+import java.util.Set;
 
 /**
  * The timer command allows users to schedule commands to occur after a certain
@@ -39,59 +41,108 @@ import com.dmdirc.ui.input.TabCompleter;
  */
 public final class TimerCommand extends Command implements IntelligentCommand,
         CommandInfo {
-    
+
+    /** The TimerManager for this TimerCommand. */
+    private final TimerManager manager;
+
     /**
-     * Creates a new instance of TimerCommand.
+     * Creates a new instance of TimerCommand and sets the internal variable manager
+     * to the TimerManager for this command.
      */
-    public TimerCommand() {
+    public TimerCommand(final TimePlugin plugin) {
         super();
+
+        this.manager = plugin.getTimerManager();
     }
-    
+
     /** {@inheritDoc} */
     @Override
     public void execute(final FrameContainer<?> origin,
             final CommandArguments args, final CommandContext context) {
-        if (args.getArguments().length < 3) {
-            doUsage(origin, args.isSilent());
+
+        if (args.getArguments().length > 0) {
+            if ("--cancel".equals(args.getArguments()[0])) {
+                int timerKey = 0;
+
+                try {
+                    timerKey = Integer.parseInt(args.getArgumentsAsString(1));
+                } catch (NumberFormatException ex) {
+                    doUsage(origin, args.isSilent());
+                    return;
+                }
+
+                if (manager.hasTimerWithID(timerKey)) {
+                    manager.getTimerByID(timerKey).cancelTimer();
+                    sendLine(origin, args.isSilent(), FORMAT_OUTPUT, "Timer cancelled");
+                } else {
+                    sendLine(origin, args.isSilent(), FORMAT_ERROR, "There is currently" +
+                            " no timer with that ID");
+                    return;
+                }
+            } else if ("--list".equals(args.getArguments()[0])) {
+                final Set<Entry<Integer, TimedCommand>> timerList
+                        = manager.listTimers();
+
+                if (!timerList.isEmpty()) {
+                    for (Entry<Integer, TimedCommand> entry : timerList) {
+                        sendLine(origin, args.isSilent(), FORMAT_OUTPUT, "Timer"
+                                + " ID: " + entry.getKey() + " - " +
+                                entry.getValue().getCommand());
+                    }
+                } else {
+                    sendLine(origin, args.isSilent(), FORMAT_ERROR, "There are " +
+                                "currently no active timers");
+                }
+            } else {
+                if (args.getArguments().length < 3) {
+                    doUsage(origin, args.isSilent());
+                } else {
+                    int repetitions = 0;
+                    int interval = 0;
+
+                    final String command = args.getArgumentsAsString(2);
+
+                    try {
+                        repetitions = Integer.parseInt(args.getArguments()[0]);
+                        interval = Integer.parseInt(args.getArguments()[1]);
+                    } catch (NumberFormatException ex) {
+                        doUsage(origin, args.isSilent());
+                        return;
+                    }
+
+                    if (interval < 1) {
+                        sendLine(origin, args.isSilent(), FORMAT_ERROR, "Cannot use" +
+                                " intervals below 1");
+                        return;
+                    }
+
+                    manager.addTimer(repetitions, interval, command,
+                            origin, context.getSource());
+
+                    sendLine(origin, args.isSilent(), FORMAT_OUTPUT, "Command scheduled.");
+                }
+            }
         } else {
-            int repetitions = 0;
-            int interval = 0;
-            final String command = args.getArgumentsAsString(2);
-            
-            try {
-                repetitions = Integer.parseInt(args.getArguments()[0]);
-                interval = Integer.parseInt(args.getArguments()[1]);
-            } catch (NumberFormatException ex) {
-                doUsage(origin, args.isSilent());
-                return;
-            }
-
-            if (interval < 1) {
-                sendLine(origin, args.isSilent(), FORMAT_ERROR, "Cannot use" +
-                        " intervals below 1");
-                return;
-            }
-
-            new TimedCommand(repetitions, interval, command, origin, context.getSource());
-            sendLine(origin, args.isSilent(), FORMAT_OUTPUT, "Command scheduled.");
+            doUsage(origin, args.isSilent());
         }
     }
-    
+
     /**
      * Displays usage information for this command.
      * @param origin The window that the command was entered in
      * @param isSilent Whether this command is being silenced or not
      */
     private void doUsage(final FrameContainer<?> origin, final boolean isSilent) {
-        showUsage(origin, isSilent, "timer", "<repetitions> <interval> <command>");
+        showUsage(origin, isSilent, "timer", "[--list|--cancel <timer id> | " +
+                "<repetitions> <interval> <command>]");
     }
-    
+
     /** {@inheritDoc} */
     @Override
     public String getName() {
         return "timer";
     }
-    
+
     /** {@inheritDoc} */
     @Override
     public boolean showInHelp() {
@@ -103,27 +154,31 @@ public final class TimerCommand extends Command implements IntelligentCommand,
     public CommandType getType() {
         return CommandType.TYPE_GLOBAL;
     }
-    
+
     /** {@inheritDoc} */
     @Override
     public String getHelp() {
-        return "timer <repetitions> <interval> <command> " +
-                "- schedules a command to be executed after a certain time";
+        return "timer [--list|--cancel <timer id> | <repetitions> <interval> <command>] " +
+                "- lists all active timers / cancells an active timer of given ID" +
+                " / schedules a command to be executed after a certain time";
     }
 
     /** {@inheritDoc} */
     @Override
     public AdditionalTabTargets getSuggestions(final int arg,
             final IntelligentCommandContext context) {
-        AdditionalTabTargets res;
-        
-        if (arg <= 1) {
-            res = new AdditionalTabTargets().excludeAll();
-        } else {
-            res = TabCompleter.getIntelligentResults(arg, context, 2);
+        final AdditionalTabTargets targets = new AdditionalTabTargets();
+
+        targets.excludeAll();
+        if (arg == 0) {
+            targets.add("--list");
+            targets.add("--cancel");
+        } else if (arg == 1 && "--cancel".equals(context.getPreviousArgs().get(0))) {
+            for (Integer i : manager.getTimerIDs()) {
+                targets.add(i.toString());
+            }
         }
-        
-        return res;
-    } 
-    
+
+        return targets;
+    }
 }
