4191cceb9d90510b7a77507445f17c0e1f53ac4c
Greg Holmes <greg@dmdirc.com>
2010-12-01 00:09:38 +0000

    Remove MDI from the client. Tidy up mainframe (creating a new dialog class). Fix OptionalJSpinner having an excessive border.
    Fixes issue CLIENT-66
    Fixes issue CLIENT-2
    
    Change-Id: I450fe4a6060988843caad0b22a1ac2ffad9e4c28
    Depends-On: If58397d37b89c8fe6e099c47fb4d26fdc4589b49

--- src/com/dmdirc/addons/ui_swing/components/MDIBar.java
+++ src/com/dmdirc/addons/ui_swing/components/MDIBar.java
@@ -1,17 +1,16 @@
 /*
- * 
  * Copyright (c) 2006-2010 Chris Smith, Shane Mc Cormack, Gregory Holmes
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,7 +26,6 @@ import com.dmdirc.FrameContainer;
 import com.dmdirc.addons.ui_swing.MainFrame;
 import com.dmdirc.addons.ui_swing.SwingController;
 import com.dmdirc.addons.ui_swing.SwingWindowListener;
-import com.dmdirc.addons.ui_swing.components.frames.TextFrame;
 import com.dmdirc.config.ConfigManager;
 import com.dmdirc.config.IdentityManager;
 import com.dmdirc.interfaces.ConfigChangeListener;
@@ -38,33 +36,38 @@ import com.dmdirc.ui.interfaces.Window;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
 
-import javax.swing.JInternalFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 import net.miginfocom.swing.MigLayout;
 
 /**
- * Provides an MDI style bar for restore/minimise/close.
+ * Provides an MDI style bar for closing frames.
  */
-public class MDIBar extends JPanel implements SwingWindowListener, SelectionListener,
-        PropertyChangeListener, ActionListener, ConfigChangeListener {
+public class MDIBar extends JPanel implements SwingWindowListener,
+        SelectionListener, ActionListener, ConfigChangeListener {
 
+    /**
+     * A version number for this class. It should be changed whenever the class
+     * structure is changed (or anything else that would prevent serialized
+     * objects being unserialized with the new class).
+     */
     private static final long serialVersionUID = -8028057596226636245L;
+    /** Icon size for the close button. */
     private static final int ICON_SIZE = 12;
-    private NoFocusButton closeButton;
-    private NoFocusButton minimiseButton;
-    private NoFocusButton restoreButton;
-    private MainFrame mainFrame;
-    private ConfigManager config;
-    private String visibility;
+    /** Button to close frames. */
+    private final NoFocusButton closeButton;
+    /** Main frame component bieng shown in. */
+    private final MainFrame mainFrame;
+    /** Config manager to get settings from. */
+    private final ConfigManager config;
+    /** Option domain. */
+    private final String configDomain;
+    /** Are we meant to be visible? */
+    private boolean visibility;
     /** Active frame. */
     private Window activeFrame;
-    /** Swing controller. */
-    private SwingController controller;
 
     /**
      * Instantiates a new MDI bar.
@@ -73,31 +76,25 @@ public class MDIBar extends JPanel implements SwingWindowListener, SelectionList
      * @param mainFrame Main frame instance
      */
     public MDIBar(final SwingController controller, final MainFrame mainFrame) {
+        super();
+
         this.mainFrame = mainFrame;
         this.config = IdentityManager.getGlobalConfig();
-        this.controller = controller;
-        visibility = config.getOption("ui", "mdiBarVisibility");
+        this.configDomain = controller.getDomain();
+        visibility = config.getOptionBool(configDomain, "mdiBarVisibility");
 
         closeButton = new NoFocusButton(IconManager.getIconManager().
                 getScaledIcon("close-12", ICON_SIZE, ICON_SIZE));
-        minimiseButton = new NoFocusButton(IconManager.getIconManager().
-                getScaledIcon("minimise-12", ICON_SIZE, ICON_SIZE));
-        restoreButton = new NoFocusButton(IconManager.getIconManager().
-                getScaledIcon("maximise-12", ICON_SIZE, ICON_SIZE));
 
         setOpaque(false);
         setLayout(new MigLayout("hmax 17, ins 1 0 0 0, fill"));
-        add(minimiseButton, "w 17!, h 17!, right");
-        add(restoreButton, "w 17!, h 17!, right");
         add(closeButton, "w 17!, h 17!, right");
 
         controller.getWindowFactory().addWindowListener(this);
 
         WindowManager.addSelectionListener(this);
         closeButton.addActionListener(this);
-        minimiseButton.addActionListener(this);
-        restoreButton.addActionListener(this);
-        config.addChangeListener("ui", "mdiBarVisibility", this);
+        config.addChangeListener(configDomain, "mdiBarVisibility", this);
 
         check();
     }
@@ -106,28 +103,19 @@ public class MDIBar extends JPanel implements SwingWindowListener, SelectionList
     @Override
     public void setEnabled(final boolean enabled) {
         closeButton.setEnabled(enabled);
-        minimiseButton.setEnabled(enabled);
-        restoreButton.setEnabled(enabled);
     }
 
+    /**
+     * Checks whether this MDI bar should be visible and active and sets itself
+     * accordingly.
+     */
     private void check() {
         SwingUtilities.invokeLater(new Runnable() {
 
             @Override
             public void run() {
-                boolean show = true;
-                if (mainFrame == null) {
-                    show = false;
-                    return;
-                } else if ("alwaysShow".equalsIgnoreCase(visibility)) {
-                    show = mainFrame.getDesktopPane().getAllFrames().length > 0;
-                } else if ("neverShow".equalsIgnoreCase(visibility)) {
-                    show = false;
-                } else if ("showWhenMaximised".equalsIgnoreCase(visibility)) {
-                    show = mainFrame.getMaximised();
-                }
-                setVisible(show);
-                setEnabled(mainFrame.getDesktopPane().getAllFrames().length > 0);
+                setVisible(visibility);
+                setEnabled(mainFrame.getActiveFrame() != null);
             }
         });
     }
@@ -135,31 +123,12 @@ public class MDIBar extends JPanel implements SwingWindowListener, SelectionList
     /** {@inheritDoc} */
     @Override
     public void windowAdded(final Window parent, final Window window) {
-        ((JInternalFrame) window).addPropertyChangeListener(
-                "maximum", this);
-
         check();
     }
 
     /** {@inheritDoc} */
     @Override
     public void windowDeleted(final Window parent, final Window window) {
-        ((JInternalFrame) window).removePropertyChangeListener(
-                this);
-
-        check();
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    public void propertyChange(final PropertyChangeEvent evt) {
-        if ((Boolean) evt.getNewValue()) {
-            restoreButton.setIcon(IconManager.getIconManager().getScaledIcon(
-                    "restore-12", ICON_SIZE, ICON_SIZE));
-        } else {
-            restoreButton.setIcon(IconManager.getIconManager().getScaledIcon(
-                    "maximise-12", ICON_SIZE, ICON_SIZE));
-        }
         check();
     }
 
@@ -175,24 +144,20 @@ public class MDIBar extends JPanel implements SwingWindowListener, SelectionList
         }
         if (closeButton.equals(e.getSource())) {
             activeFrame.close();
-        } else if (minimiseButton.equals(e.getSource())) {
-            ((TextFrame) activeFrame).minimise();
-        } else if (restoreButton.equals(e.getSource())) {
-            activeFrame.toggleMaximise();
         }
     }
 
     /** {@inheritDoc} */
     @Override
     public void configChanged(final String domain, final String key) {
-        visibility = config.getOption("ui", "mdiBarVisibility");
+        visibility = config.getOptionBool(configDomain, "mdiBarVisibility");
         check();
     }
 
     /** {@inheritDoc} */
     @Override
     public void selectionChanged(final FrameContainer<?> window) {
-        activeFrame = controller.getWindowFactory().getSwingWindow(window);
+        activeFrame = mainFrame.getActiveFrame();
         check();
     }
 }
