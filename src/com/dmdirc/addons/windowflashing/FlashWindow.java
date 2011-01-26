68d619ad3cb0b3edd8a8290fdcd28c157dd56096
Greg Holmes <greg@dmdirc.com>
2011-01-26 15:30:02 +0000

    Add plugin to flash taskbar on windows
    Depends-On: Iabc748b6d190b218f771c62a9e713259f81dcc9e
    Depends-On: Ief644141e53c63aea5f1e7a405d1623fca5fbd10
    Change-Id: Ia4e2bf42b18f88b2138a03a2484e903bfe67df15

---
+++ src/com/dmdirc/addons/windowflashing/FlashWindow.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2006-2011 Chris Smith, Shane Mc Cormack, Gregory Holmes
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc.addons.windowflashing;
+
+import com.dmdirc.FrameContainer;
+import com.dmdirc.commandparser.CommandArguments;
+import com.dmdirc.commandparser.CommandInfo;
+import com.dmdirc.commandparser.CommandType;
+import com.dmdirc.commandparser.commands.Command;
+import com.dmdirc.commandparser.commands.context.CommandContext;
+
+/**
+ * Command to flash an inactive window under Windows.
+ */
+public class FlashWindow extends Command implements CommandInfo {
+    
+    private final WindowFlashing plugin;
+
+    /**
+     * Creates a new flash window command.
+     *
+     * @param plugin Parent plugin to trigger window flashing
+     */
+    public FlashWindow(final WindowFlashing plugin) {
+        super();
+        this.plugin = plugin;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void execute(final FrameContainer<?> origin,
+            final CommandArguments args, final CommandContext context) {
+        plugin.flashWindow();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String getName() {
+        return "flashwindow";
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public boolean showInHelp() {
+        return true;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String getHelp() {
+        return "flashwindow - Flashes the window until activated";
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public CommandType getType() {
+        return CommandType.TYPE_GLOBAL;
+    }
+
+}
