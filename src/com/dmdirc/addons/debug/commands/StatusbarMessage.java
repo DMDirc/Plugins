189e7771e05a8f86f2ffd10ff872510904fe541f
Greg Holmes <greg@dmdirc.com>
2011-01-29 21:29:58 +0000

    Add basic status bar message debug command
    Change-Id: I2cf9101abbd0bd7b717579443a61b6d3286b34d6

---
+++ src/com/dmdirc/addons/debug/commands/StatusbarMessage.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2006-2011 Chris Smith, Shane Mc Cormack, Gregory Holmes
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc.addons.debug.commands;
+
+import com.dmdirc.FrameContainer;
+import com.dmdirc.addons.debug.Debug;
+import com.dmdirc.addons.debug.DebugCommand;
+import com.dmdirc.commandparser.CommandArguments;
+import com.dmdirc.commandparser.commands.context.CommandContext;
+import com.dmdirc.config.IdentityManager;
+import com.dmdirc.ui.StatusMessage;
+import com.dmdirc.ui.core.components.StatusBarManager;
+
+/**
+ * Outputs a test message to the status bar.
+ */
+public class StatusbarMessage extends DebugCommand {
+
+    /**
+     * Creates a new instance of the command.
+     *
+     * @param command Parent command
+     */
+    public StatusbarMessage(final Debug command) {
+        super(command);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String getName() {
+        return "statusmessage";
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String getUsage() {
+        return "message - Sets the status bar message";
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void execute(final FrameContainer<?> origin,
+            final CommandArguments args, final CommandContext context) {
+        StatusBarManager.getStatusBarManager().setMessage(new StatusMessage(
+                null, "Test: " + args.getArgumentsAsString(), null, 5,
+                IdentityManager.getGlobalConfig()));
+    }
+}
