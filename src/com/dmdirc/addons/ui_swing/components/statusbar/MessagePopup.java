5172abf42c5de1f40509f7104235eded858dfd47
Greg Holmes <greg@dmdirc.com>
2011-01-30 23:03:03 +0000

    Make status bar message history popup the same size as the message label.
    Fixes CLIENT-211
    
    Change-Id: I4e0165c74b8f7c3c84a23b659aea341e122e849d

--- src/com/dmdirc/addons/ui_swing/components/statusbar/MessagePopup.java
+++ src/com/dmdirc/addons/ui_swing/components/statusbar/MessagePopup.java
@@ -24,6 +24,8 @@ package com.dmdirc.addons.ui_swing.components.statusbar;
 
 import com.dmdirc.ui.IconManager;
 import com.dmdirc.ui.StatusMessage;
+import java.awt.Dimension;
+import java.awt.Point;
 
 import java.awt.Window;
 import java.awt.event.MouseEvent;
@@ -40,23 +42,31 @@ import javax.swing.UIManager;
  */
 class MessagePopup extends StatusbarTogglePanel<JLabel> {
 
+    /**
+     * A version number for this class. It should be changed whenever the class
+     * structure is changed (or anything else that would prevent serialized
+     * objects being unserialized with the new class).
+     */
+    private static final long serialVersionUID = 2;
     /** Parent window. */
     private final Window parentWindow;
     /** List of historical messages. */
     private final List<StatusMessage> messages;
+    /** Parent panel. */
+    private final JPanel parent;
 
     /**
      * Creates a new message history popup.
      *
-     * @param nonSelectedBorder Border when unselected
-     * @param selectedBorder Border when selected
-     * @param mainFrame Mainframe
+     * @param parent Parent to size against
+     * @param parentWindow Parent window    
      */
-    public MessagePopup(final Window parentWindow) {
+    public MessagePopup(final JPanel parent, final Window parentWindow) {
         super(new JLabel("^"),
                 new SidelessEtchedBorder(SidelessEtchedBorder.Side.LEFT),
                 new SidelessEtchedBorder(SidelessEtchedBorder.Side.TOP));
         this.parentWindow = parentWindow;
+        this.parent = parent;
         messages = new ArrayList<StatusMessage>();
     }
 
@@ -103,6 +113,13 @@ class MessagePopup extends StatusbarTogglePanel<JLabel> {
     private class MessageHistoryPanel extends StatusbarPopupWindow {
 
         /**
+         * A version number for this class. It should be changed whenever the
+         * class structure is changed (or anything else that would prevent
+         * serialized objects being unserialized with the new class).
+         */
+        private static final long serialVersionUID = 2;
+
+        /**
          * Creates a new message history window.
          *
          * @param window Parent window
@@ -115,6 +132,7 @@ class MessagePopup extends StatusbarTogglePanel<JLabel> {
         @Override
         protected void initContent(final JPanel panel) {
             panel.removeAll();
+            panel.setPreferredSize(new Dimension(parent.getSize()));
             if (messages.isEmpty()) {
                 panel.add(new JLabel("No previous messages."), "grow, push");
                 return;
@@ -126,6 +144,20 @@ class MessagePopup extends StatusbarTogglePanel<JLabel> {
                         message.getIconType()), SwingConstants.LEFT),
                         "grow, push, wrap");
             }
+        }
+
+        /* {@inheritDoc} */
+        @Override
+        protected Point getPopupLocation() {
+            final Point point = parent.getLocationOnScreen();
+            point.translate(parent.getWidth() / 2 - this.getWidth() / 2 - 2,
+                    - this.getHeight() + 1);
+            final int maxX = Math.max(parentWindow.getLocationOnScreen().x
+                    + parentWindow.getWidth() - 10 - getWidth(),
+                    parent.getLocationOnScreen().x + parent.getWidth() - 1
+                    - getWidth());
+            point.x = Math.min(maxX, point.x + 1);
+            return point;
         }
     }
 }
