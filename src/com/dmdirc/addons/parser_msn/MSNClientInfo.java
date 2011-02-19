4a2aef41c7c2fc426d85ae15aef494836e6e8acb
Greg Holmes <greg@dmdirc.com>
2011-02-19 02:04:57 +0000

    Add initial work on MSN parser
    Change-Id: Ie684811c98597cabdfa608786763675b491444fc
    Depends-On: Icaa1cebfd34ddc1a278d00468d9d1da3048eb869

---
+++ src/com/dmdirc/addons/parser_msn/MSNClientInfo.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2006-2010 Chris Smith, Shane Mc Cormack, Gregory Holmes
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc.addons.parser_msn;
+
+import com.dmdirc.parser.common.AwayState;
+import com.dmdirc.parser.common.BaseClientInfo;
+
+import net.sf.jml.MsnUser;
+import net.sf.jml.MsnUserStatus;
+
+/**
+ * An MSN-specific client info object.
+ */
+public class MSNClientInfo extends BaseClientInfo {
+
+    /** MSN contact. */
+    private MsnUser contact;
+
+    /**
+     * Creates a new client info object with the specified details.
+     *
+     * @param contact The contact this object represents
+     * @param parser The parser that owns this client info object
+     * @param nick The nickname of the user this object represents
+     * @param user The username of the user this object represents
+     * @param host The hostname of the user this object represents
+     */
+    public MSNClientInfo(final MsnUser contact, final MSNParser parser,
+            final String nick, final String user, final String host) {
+        super(parser, nick, user, host);
+
+        this.contact = contact;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public int getChannelCount() {
+        return 0;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public AwayState getAwayState() {
+        if (contact.getStatus() == MsnUserStatus.ONLINE) {
+            return AwayState.HERE;
+        }
+        return AwayState.AWAY;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public MSNParser getParser() {
+        return (MSNParser) super.getParser();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String getRealname() {
+        return contact.getDisplayName();
+    }
+
+    /**
+     * Returns the contact for this client info object
+     *
+     * @return MSN contact for this client
+     */
+    public MsnUser getContact() {
+        return contact;
+    }
+
+}
---
+++ src/com/dmdirc/addons/parser_msn/MSNLocalClientInfo.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2006-2010 Chris Smith, Shane Mc Cormack, Gregory Holmes
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc.addons.parser_msn;
+
+import com.dmdirc.parser.common.AwayState;
+import com.dmdirc.parser.interfaces.LocalClientInfo;
+import com.dmdirc.parser.interfaces.callbacks.AwayStateListener;
+
+import net.sf.jml.MsnOwner;
+import net.sf.jml.MsnUserStatus;
+
+/**
+ * A representation of the local MSN client.
+ */
+public class MSNLocalClientInfo extends MSNClientInfo implements LocalClientInfo {
+
+    /** MSN user. */
+    private final MsnOwner owner;
+
+    /**
+     * Creates a new local client info object with the specified details.
+     *
+     * @param owner The client this object represents
+     * @param parser The parser that owns this client info object
+     * @param nick The nickname of the user this object represents
+     * @param user The username of the user this object represents
+     * @param host The hostname of the user this object represents
+     */
+    public MSNLocalClientInfo(final MsnOwner owner, final MSNParser parser,
+            final String nick, final String user, final String host) {
+        super(owner, parser, nick, user, host);
+
+        this.owner = owner;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void setNickname(final String name) {
+        owner.setDisplayName(name);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String getModes() {
+        return "";
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void setAway(final String reason) {
+        owner.setStatus(MsnUserStatus.AWAY);
+
+        getParser().getCallbackManager().getCallbackType(
+                AwayStateListener.class).call(AwayState.HERE, AwayState.AWAY,
+                reason);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void setBack() {
+        owner.setStatus(MsnUserStatus.ONLINE);
+
+        getParser().getCallbackManager().getCallbackType(
+                AwayStateListener.class).call(AwayState.AWAY, AwayState.HERE,
+                null);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void alterMode(final boolean add, final Character mode) {
+        //Ignore
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void flushModes() {
+        //Ignore
+    }
+
+}
