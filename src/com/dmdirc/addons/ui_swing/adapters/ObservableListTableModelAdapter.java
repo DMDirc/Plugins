b52f19508bb10461708cc247d1501d42171c6967
Chris Smith <chris@dmdirc.com>
2011-12-04 21:00:02 +0000

    Table model for the channel list dialog
    Change-Id: I5a5afd930047e33506b0c4518836f3a7b4454062

---
+++ src/com/dmdirc/addons/ui_swing/adapters/ObservableListTableModelAdapter.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2006-2011 DMDirc Developers
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc.addons.ui_swing.adapters;
+
+import com.dmdirc.util.ListObserver;
+import com.dmdirc.util.ObservableList;
+
+import javax.swing.table.AbstractTableModel;
+
+/**
+ * Adapts an observable list for use as a table model.
+ *
+ * @param <T> The type of object in the list
+ */
+public abstract class ObservableListTableModelAdapter<T> extends AbstractTableModel {
+
+    /** Serial version UID. */
+    private static final long serialVersionUID = 1L;
+
+    /** The list we're observing. */
+    protected final ObservableList<T> list;
+
+    /**
+     * Creates a new {@link ObservableListTableModelAdapter} backed by the
+     * given list.
+     *
+     * @param list The backing list for this model
+     */
+    public ObservableListTableModelAdapter(final ObservableList<T> list) {
+        this.list = list;
+        this.list.addListListener(new Listener());
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public int getRowCount() {
+        return this.list.size();
+    }
+
+    /**
+     * List observer which fires the relevant methods to notify this model's
+     * listeners.
+     */
+    private class Listener implements ListObserver {
+
+        /** {@inheritDoc} */
+        @Override
+        public void onItemsAdded(final Object source, final int startIndex,
+                final int endIndex) {
+            fireTableRowsInserted(startIndex, endIndex);
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public void onItemsRemoved(final Object source, final int startIndex,
+                final int endIndex) {
+            fireTableRowsDeleted(startIndex, endIndex);
+        }
+
+        /** {@inheritDoc} */
+        @Override
+        public void onItemsChanged(final Object source, final int startIndex,
+                final int endIndex) {
+            fireTableRowsUpdated(startIndex, endIndex);
+        }
+    }
+
+}
---
+++ src/com/dmdirc/addons/ui_swing/dialogs/channellist/ChannelListTableModel.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2006-2011 DMDirc Developers
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc.addons.ui_swing.dialogs.channellist;
+
+import com.dmdirc.addons.ui_swing.adapters.ObservableListTableModelAdapter;
+import com.dmdirc.lists.GroupListEntry;
+import com.dmdirc.lists.GroupListManager;
+
+/**
+ * Table model for channel list results.
+ */
+public class ChannelListTableModel extends ObservableListTableModelAdapter<GroupListEntry> {
+
+    /** Serial version UID. */
+    private static final long serialVersionUID = 1L;
+
+    /** The names to use for the model's columns. */
+    private static final String[] COLUMN_NAMES = new String[] {
+        "Name", "Users", "Topic"
+    };
+
+    /** The types to use for the model's columns. */
+    @SuppressWarnings("rawtypes")
+    private static final Class[] COLUMN_TYPES = new Class[] {
+        String.class, Integer.class, String.class
+    };
+
+    /**
+     * Creates a new table model backed by the given manager.
+     *
+     * @param manager The manager to use to retrieve group list entries.
+     */
+    public ChannelListTableModel(final GroupListManager manager) {
+        super(manager.getGroups());
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public int getColumnCount() {
+        return 3;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Object getValueAt(final int rowIndex, final int columnIndex) {
+        final GroupListEntry entry = list.get(rowIndex);
+
+        switch (columnIndex) {
+            case 0:
+                return entry.getName();
+            case 1:
+                return entry.getUsers();
+            case 2:
+                return entry.getTopic();
+        }
+
+        return null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Class<?> getColumnClass(final int columnIndex) {
+        return COLUMN_TYPES[columnIndex];
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String getColumnName(final int column) {
+        return COLUMN_NAMES[column];
+    }
+
+}
