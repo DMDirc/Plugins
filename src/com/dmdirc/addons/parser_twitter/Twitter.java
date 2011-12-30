3a256b9a78f9f8849ce80692e6063f8e2387aedf
Shane Mc Cormack <shanemcc@gmail.com>
2011-12-30 20:42:02 +0000

    Fix for issue CLIENT-312 - Parser shouldn't extend runnable
    The parser now defines some new methods:
      - connect()
      - connect(threadName)
      - interrupt()
    
    connect([threadName]) is responsible for creating any required threads
    for the parser to run in and maintaining them internally.
    interrupt() allows for interrupting these thread if they exist and if
    required.
    
    A new base class for parsers has been added to accomodate this with
    minimum changes required elsewhere - ThreadedParser which BaseParser
    now extends.
    
    ThreadedParser handles the creation of a single controlThread and allows
    interrupting it, and also defines an abstract void run() method (which
    all existing parsers will already have from extending runnable in the
    past) which is used by the controlThread.
    
    Change-Id: I90ac61d56eb623165d3c4211f1c3247fcba823c8
    Depends-On: Ib3e884407181adc362394b23fe59f8e3b401d692
    Depends-On: I5ef8ea1e988fe9591df0054ad0016c409fc34b3f

--- src/com/dmdirc/addons/parser_twitter/Twitter.java
+++ src/com/dmdirc/addons/parser_twitter/Twitter.java
@@ -39,11 +39,11 @@ import com.dmdirc.parser.common.CompositionState;
 import com.dmdirc.parser.common.DefaultStringConverter;
 import com.dmdirc.parser.common.IgnoreList;
 import com.dmdirc.parser.common.QueuePriority;
+import com.dmdirc.parser.common.ThreadedParser;
 import com.dmdirc.parser.interfaces.ChannelClientInfo;
 import com.dmdirc.parser.interfaces.ChannelInfo;
 import com.dmdirc.parser.interfaces.ClientInfo;
 import com.dmdirc.parser.interfaces.LocalClientInfo;
-import com.dmdirc.parser.interfaces.Parser;
 import com.dmdirc.parser.interfaces.StringConverter;
 import com.dmdirc.parser.interfaces.callbacks.AuthNoticeListener;
 import com.dmdirc.parser.interfaces.callbacks.ChannelJoinListener;
@@ -85,7 +85,7 @@ import java.util.Map;
 /**
  * Twitter Parser for DMDirc.
  */
-public class Twitter implements Parser, TwitterErrorHandler, TwitterRawHandler,
+public class Twitter extends ThreadedParser implements TwitterErrorHandler, TwitterRawHandler,
         ConfigChangeListener {
 
     /** Number of loops between clearing of the status cache. */
