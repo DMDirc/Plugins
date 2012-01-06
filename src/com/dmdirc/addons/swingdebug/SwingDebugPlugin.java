2301d1a9e44c1d721ad03ed36d5f92ba5324f3a7
Greg Holmes <greg@dmdirc.com>
2012-01-06 20:32:03 +0000

    Change Swing debug plugin to use DMDirc windows rather than dialogs.
    Change-Id: Ic29bf3495781213b4114769b6ef11013352d5a12

--- src/com/dmdirc/addons/swingdebug/SwingDebugPlugin.java
+++ src/com/dmdirc/addons/swingdebug/SwingDebugPlugin.java
@@ -19,31 +19,23 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-
 package com.dmdirc.addons.swingdebug;
 
 import com.dmdirc.addons.ui_swing.DMDircEventQueue;
 import com.dmdirc.addons.ui_swing.SwingController;
 import com.dmdirc.addons.ui_swing.components.CheckBoxMenuItem;
-import com.dmdirc.addons.ui_swing.components.text.TextLabel;
 import com.dmdirc.plugins.BasePlugin;
 
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.IOException;
-import java.io.PrintStream;
 
 import javax.swing.JCheckBoxMenuItem;
-import javax.swing.JDialog;
 import javax.swing.JMenu;
-import javax.swing.JScrollPane;
-
-import net.miginfocom.swing.MigLayout;
 
 /**
- * Swing debug plugin. Provides long running EDT task violation detection and
- * a console for System.out and System.err.
+ * Swing debug plugin. Provides long running EDT task violation detection and a
+ * console for System.out and System.err.
  */
 public class SwingDebugPlugin extends BasePlugin implements ActionListener {
 
@@ -57,18 +49,10 @@ public class SwingDebugPlugin extends BasePlugin implements ActionListener {
     private JCheckBoxMenuItem showSysOut;
     /** Debug EDT menu item. */
     private JCheckBoxMenuItem showSysErr;
-    /** Old System.out. */
-    private PrintStream sysout;
-    /** Old System.err. */
-    private PrintStream syserr;
-    /** System.out redirect thread. */
-    private SystemStreamRedirectThread out;
-    /** System.err redirect thread. */
-    private SystemStreamRedirectThread err;
-    /** System out frame. */
-    private JDialog outFrame;
-    /** System error frame. */
-    private JDialog errorFrame;
+    /** System out window. */
+    private SystemStreamContainer sysoutFrame;
+    /** System error window. */
+    private SystemStreamContainer syserrFrame;
 
     /**
      * Creates a new SwingDebugPlugin.
@@ -82,56 +66,22 @@ public class SwingDebugPlugin extends BasePlugin implements ActionListener {
     /** {@inheritDoc} */
     @Override
     public void onLoad() {
-        try {
-            sysout = System.out;
-            syserr = System.err;
-            final TextLabel outTextArea = new TextLabel();
-            final TextLabel errorTextArea = new TextLabel();
-            outFrame = new JDialog(controller.getMainFrame());
-            errorFrame = new JDialog(controller.getMainFrame());
-            outFrame.setTitle("DMDirc: System.out Console");
-            errorFrame.setTitle("DMDirc: System.err Console");
-            outFrame.setLayout(new MigLayout("ins 0, pack, wmin 20sp, hmin 20sp"));
-            errorFrame.setLayout(new MigLayout("ins 0, pack, wmin 20sp, hmin 20sp"));
-            outFrame.add(new JScrollPane(outTextArea), "grow, push");
-            errorFrame.add(new JScrollPane(errorTextArea), "grow, push");
-            outFrame.pack();
-            errorFrame.pack();
-            out = new SystemStreamRedirectThread(
-                    SystemStreamRedirectThread.Stream.OUT,
-                    outTextArea.getDocument());
-            err = new SystemStreamRedirectThread(
-                    SystemStreamRedirectThread.Stream.IN,
-                    errorTextArea.getDocument());
-            out.start();
-            err.start();
-            debugMenu = new JMenu("Debug");
-            debugEDT = new CheckBoxMenuItem("Check EDT task length");
-            showSysOut = new CheckBoxMenuItem("Show System.out Console");
-            showSysErr = new CheckBoxMenuItem("Show System.err Console");
-            debugEDT.addActionListener(this);
-            showSysErr.addActionListener(this);
-            showSysOut.addActionListener(this);
-            controller.getMainFrame().getJMenuBar().add(debugMenu);
-            debugMenu.add(debugEDT);
-            debugMenu.add(showSysOut);
-            debugMenu.add(showSysErr);
-            CloseListener.add(showSysOut.getModel(), outFrame);
-            CloseListener.add(showSysErr.getModel(), outFrame);
-        } catch (IOException ex) {
-            onUnload();
-        }
+        debugMenu = new JMenu("Debug");
+        debugEDT = new CheckBoxMenuItem("Check EDT task length");
+        showSysOut = new CheckBoxMenuItem("Show System.out Console");
+        showSysErr = new CheckBoxMenuItem("Show System.err Console");
+        debugEDT.addActionListener(this);
+        showSysErr.addActionListener(this);
+        showSysOut.addActionListener(this);
+        controller.getMainFrame().getJMenuBar().add(debugMenu);
+        debugMenu.add(debugEDT);
+        debugMenu.add(showSysOut);
+        debugMenu.add(showSysErr);
     }
 
     /** {@inheritDoc} */
     @Override
     public void onUnload() {
-        out.cancel();
-        err.cancel();
-        System.setOut(sysout);
-        System.setErr(syserr);
-        outFrame = null;
-        errorFrame = null;
         controller.getMainFrame().getJMenuBar().remove(debugMenu);
     }
 
@@ -152,11 +102,35 @@ public class SwingDebugPlugin extends BasePlugin implements ActionListener {
             }
         }
         if (e.getSource() == showSysOut) {
-            outFrame.setVisible(showSysOut.getState());
+            if (showSysOut.isSelected()) {
+                sysoutFrame = new SystemStreamContainer(SystemStreamType.Out,
+                        controller.getGlobalConfig(), this);
+            } else {
+                sysoutFrame.close();
+            }
         }
+
         if (e.getSource() == showSysErr) {
-            errorFrame.setVisible(showSysErr.getState());
+            if (showSysErr.isSelected()) {
+                syserrFrame = new SystemStreamContainer(SystemStreamType.Error,
+                        controller.getGlobalConfig(), this);
+            } else {
+                syserrFrame.close();
+            }
         }
     }
 
+    /**
+     * Notifies this plugin the specified container is closing.
+     *
+     * @param container Container that is closing
+     */
+    void windowClosing(final SystemStreamContainer container) {
+        if (container == sysoutFrame) {
+            showSysOut.setSelected(false);
+        }
+        if (container == syserrFrame) {
+            showSysErr.setSelected(false);
+        }
+    }
 }
---
+++ src/com/dmdirc/addons/swingdebug/SystemStreamContainer.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2006-2012 DMDirc Developers
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.dmdirc.addons.swingdebug;
+
+import com.dmdirc.FrameContainer;
+import com.dmdirc.Server;
+import com.dmdirc.config.ConfigManager;
+import com.dmdirc.ui.WindowManager;
+import com.dmdirc.ui.core.components.WindowComponent;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ * Redirects either System.out or System.err to a window in the client.
+ */
+public class SystemStreamContainer extends FrameContainer {
+
+    /** Stream reader thread. */
+    private SystemStreamRedirectThread thread;
+    /** Parent plugin. */
+    private SwingDebugPlugin plugin;
+
+    /**
+     * Creates a new system stream container wrapping the specified stream.
+     *
+     * @param stream Stream to wrap
+     * @param config Config to wrap
+     * @param plugin Parent plugin
+     */
+    public SystemStreamContainer(final SystemStreamType stream,
+            final ConfigManager config,
+            final SwingDebugPlugin plugin) {
+        super("dmdirc", stream.toString(), stream.toString(), config,
+                Arrays.asList(WindowComponent.TEXTAREA.getIdentifier()));
+        this.plugin = plugin;
+        try {
+            thread = new SystemStreamRedirectThread(stream, getDocument());
+            thread.start();
+        } catch (IOException ex) {
+        }
+        WindowManager.getWindowManager().addWindow(this);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public Server getServer() {
+        return getParent() == null ? null : getParent().getServer();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    protected void windowClosing() {
+        thread.cancel();
+        plugin.windowClosing(this);
+        // 2: Remove any callbacks or listeners
+        // 3: Trigger any actions neccessary
+        // 4: Trigger action for the window closing
+        // 5: Inform any parents that the window is closing
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void windowClosed() {
+        // 7: Remove any references to the window and parents
+    }
+
+}
