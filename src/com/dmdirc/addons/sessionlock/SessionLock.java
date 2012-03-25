98d9984fa734cd7c0c37ed4512824fed0fbaa2b1
Greg Holmes <greg@dmdirc.com>
2012-03-25 23:23:51 +0100

    Add Windows Session Lock plugin.
    Change-Id: I4f31edd67867d4039268b3568821a4bd5b0779b5

---
+++ src/com/dmdirc/addons/sessionlock/SessionLock.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (c) 2006-2012 DMDirc Developers
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.dmdirc.addons.sessionlock;
+
+import com.dmdirc.actions.ActionManager;
+import com.dmdirc.plugins.BasePlugin;
+
+import com.greboid.lock.LockAdapter;
+import com.greboid.lock.LockListener;
+
+/**
+ * Plugin that detects Session lock/unlock events.
+ */
+public class SessionLock extends BasePlugin implements LockListener {
+
+    /**
+     * Have we registered our actions?
+     */
+    private static boolean registered;
+    /**
+     * Lock Adapter to detect session events.
+     */
+    private LockAdapter lockAdapter;
+    /**
+     * Action manager.
+     */
+    private final ActionManager actionManager;
+
+    /**
+     * Creates a new session lock plugin.
+     *
+     * @param actionManager Action manager
+     */
+    public SessionLock(final ActionManager actionManager) {
+        this.actionManager = actionManager;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void onLoad() {
+        super.onLoad();
+
+        if (!registered) {
+            actionManager.registerTypes(SessionLockActionType.values());
+            registered = true;
+        }
+
+        lockAdapter = new LockAdapter();
+        lockAdapter.addLockListener(this);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void onUnload() {
+        super.onUnload();
+
+        lockAdapter.removeLockListener(this);
+        lockAdapter = null;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void locked() {
+        actionManager.triggerEvent(SessionLockActionType.SESSION_LOCK, null);
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void unlocked() {
+        actionManager.triggerEvent(SessionLockActionType.SESSION_UNLOCK, null);
+    }
+
+}
---
+++ src/com/dmdirc/addons/sessionlock/SessionLockActionType.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2006-2012 DMDirc Developers
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.dmdirc.addons.sessionlock;
+
+import com.dmdirc.interfaces.actions.ActionMetaType;
+import com.dmdirc.interfaces.actions.ActionType;
+
+/**
+ * Session lock action types.
+ */
+public enum SessionLockActionType implements ActionType {
+
+    /** Indicates the session is locked. */
+    SESSION_LOCK("Session locked"),
+    /** Indicated the session is unlocked. */
+    SESSION_UNLOCK("Session unlocked");
+
+    /** Type name. */
+    private final String name;
+
+    /**
+     * Creates a new instance of this action type.
+     *
+     * @param name Name
+     */
+    SessionLockActionType(final String name) {
+        this.name = name;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public ActionMetaType getType() {
+        return SessionLockActionMetaType.SESSION_EVENT;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public String getName() {
+        return name;
+    }
+
+}
