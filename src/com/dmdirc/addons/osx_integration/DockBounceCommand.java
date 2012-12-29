94d3d4405e17ad51e55ab91559e9859570d23f69
Chris Northwood <chris@pling.org.uk>
2012-12-29 20:24:04 +0000

    Add command to bounce dock icon on OSX
    Change-Id: I4f0ae98d92e106e72388b81e1c7747f9a7ed68a6

---
+++ src/com/dmdirc/addons/osx_integration/DockBounceCommand.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2006-2012 DMDirc Developers
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc.addons.osx_integration;
+
+import com.dmdirc.FrameContainer;
+import com.dmdirc.addons.ui_swing.SwingController;
+import com.dmdirc.commandparser.BaseCommandInfo;
+import com.dmdirc.commandparser.CommandArguments;
+import com.dmdirc.commandparser.CommandType;
+import com.dmdirc.commandparser.commands.Command;
+import com.dmdirc.commandparser.commands.context.CommandContext;
+
+/**
+ *
+ */
+public class DockBounceCommand extends Command {
+
+    /**
+     * Information about this command.
+     */
+    public static final BaseCommandInfo INFO = new BaseCommandInfo(
+            "dockbounce",
+            "dockbounce - bounce the dock icon on OS X",
+            CommandType.TYPE_GLOBAL
+    );
+
+    private final SwingController uiController;
+
+    /**
+     * @param uiController The controller for the UI which will bounce in the
+     *                     dock
+     */
+    public DockBounceCommand(final SwingController uiController) {
+        this.uiController = uiController;
+    }
+
+    @Override
+    public void execute(final FrameContainer origin,
+                        final CommandArguments args,
+                        final CommandContext context) {
+        uiController.getApple().requestUserAttention(false);
+    }
+}
