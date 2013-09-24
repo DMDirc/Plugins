a8afcf43db8af840481b1b62944d9ae402273d5a
Greg Holmes <greg@dmdirc.com>
2013-09-24 19:09:37 +0100

    Remove Unity support.
    Change-Id: Ia427f2bcdd11a28d2822a8f3f199f1e020e79072

--- src/com/dmdirc/addons/ui_swing/MainFrame.java
+++ src/com/dmdirc/addons/ui_swing/MainFrame.java
@@ -68,9 +68,6 @@ import lombok.extern.slf4j.Slf4j;
 
 import net.miginfocom.swing.MigLayout;
 
-import org.java.ayatana.ApplicationMenu;
-import org.java.ayatana.AyatanaDesktop;
-
 /**
  * The main application frame.
  */
@@ -137,9 +134,6 @@ public final class MainFrame extends JFrame implements WindowListener,
 
         focusOrder = new QueuedLinkedHashSet<>();
         initComponents();
-        if (AyatanaDesktop.isSupported()) {
-            ApplicationMenu.tryInstall(this, getJMenuBar(), new UnityMenu());
-        }
 
         imageIcon = new ImageIcon(new IconManager(controller.getGlobalConfig())
                 .getImage("icon"));
