516f190942e1a9af9e07c34472a6a072da7b8c5c
Greg Holmes <greg@dmdirc.com>
2013-10-31 23:26:21 +0000

    Add test for creating a new profile
    Change-Id: Ie888cc35ccae052f73b92885c3fbb17d00c3f529
    Reviewed-on: http://gerrit.dmdirc.com/2958
    Automatic-Compile: DMDirc Build Manager
    Reviewed-by: Chris Smith <chris@dmdirc.com>

--- test/com/dmdirc/addons/ui_swing/dialogs/profiles/ProfileTest.java
+++ test/com/dmdirc/addons/ui_swing/dialogs/profiles/ProfileTest.java
@@ -33,8 +33,11 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * Test profile class.
@@ -159,6 +162,17 @@ public class ProfileTest {
         verify(configProvider).setOption("profile", "nicknames", nicknames);
         verify(configProvider).setOption("profile", "realname", "realname");
         verify(configProvider).setOption("profile", "ident", "ident");
+    }
+
+    @Test
+    public void testSaveNoConfig() {
+        final ConfigProvider configProvider = mock(ConfigProvider.class);
+
+        when(identityFactory.createProfileConfig("New Profile")).thenReturn(configProvider);
+
+        Profile instance = new Profile(identityFactory, null);
+        instance.save();
+        verify(identityFactory).createProfileConfig("New Profile");
     }
 
     /**
