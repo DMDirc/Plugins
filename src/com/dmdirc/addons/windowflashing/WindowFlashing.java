9f74ad213fe843560da6f58d8a1dd36483c19d5a
Greg Holmes <greg@dmdirc.com>
2014-02-17 20:00:28 +0000

    DI the window flashing plugin.
    Change-Id: I891b7a42d0d924ab518006fb297f4ffeb4807f85
    Reviewed-on: http://gerrit.dmdirc.com/3113
    Automatic-Compile: DMDirc Build Manager
    Reviewed-by: Chris Smith <chris@dmdirc.com>

--- src/com/dmdirc/addons/windowflashing/WindowFlashing.java
+++ src/com/dmdirc/addons/windowflashing/WindowFlashing.java
@@ -22,93 +22,39 @@
 
 package com.dmdirc.addons.windowflashing;
 
-import com.dmdirc.actions.ActionManager;
-import com.dmdirc.actions.CoreActionType;
-import com.dmdirc.addons.ui_swing.MainFrame;
-import com.dmdirc.addons.ui_swing.SwingController;
-import com.dmdirc.config.ConfigBinder;
-import com.dmdirc.config.ConfigBinding;
 import com.dmdirc.config.prefs.PluginPreferencesCategory;
 import com.dmdirc.config.prefs.PreferencesCategory;
 import com.dmdirc.config.prefs.PreferencesDialogModel;
 import com.dmdirc.config.prefs.PreferencesSetting;
 import com.dmdirc.config.prefs.PreferencesType;
-import com.dmdirc.interfaces.ActionListener;
-import com.dmdirc.interfaces.CommandController;
-import com.dmdirc.interfaces.actions.ActionType;
-import com.dmdirc.interfaces.config.IdentityController;
 import com.dmdirc.plugins.Exported;
 import com.dmdirc.plugins.PluginInfo;
 import com.dmdirc.plugins.implementations.BaseCommandPlugin;
 
-import com.sun.jna.Native;
-import com.sun.jna.NativeLibrary;
-import com.sun.jna.Pointer;
-import com.sun.jna.platform.win32.User32;
-import com.sun.jna.platform.win32.WinDef.HWND;
-import com.sun.jna.platform.win32.WinUser;
-import com.sun.jna.platform.win32.WinUser.FLASHWINFO;
+import dagger.ObjectGraph;
 
 /**
  * Native notification plugin to make DMDirc support windows task bar flashing.
  */
-public class WindowFlashing extends BaseCommandPlugin implements ActionListener {
+public class WindowFlashing extends BaseCommandPlugin {
 
+    /** Window flashing manager. */
+    private WindowFlashingManager manager;
     /** This plugin's plugin info. */
-    private final PluginInfo pluginInfo;
-    /** Config binder. */
-    private final ConfigBinder binder;
-    /** Parent swing controller. */
-    private final SwingController controller;
-    /** Action manager used for getting client events. */
-    private final ActionManager actionManager;
-    /** Library instance. */
-    private User32 user32;
-    /** Swing main frame. */
-    private MainFrame mainFrame;
-    /** Cached blink rate setting. */
-    @ConfigBinding(domain="plugin-windowflashing", key="blinkrate",
-            fallbacks={"plugin-windowflashing", "blinkratefallback"})
-    private int blinkrate;
-    /** Cached count setting. */
-    @ConfigBinding(domain="plugin-windowflashing", key="flashcount",
-            fallbacks={"plugin-windowflashing", "flashcountfallback"})
-    private int flashcount;
-    /** Cached flash taskbar setting. */
-    @ConfigBinding(domain="plugin-windowflashing", key="flashtaskbar")
-    private boolean flashtaskbar;
-    /** Cached flash caption setting. */
-    @ConfigBinding(domain="plugin-windowflashing", key="flashcaption")
-    private boolean flashcaption;
+    private PluginInfo pluginInfo;
 
-    /**
-     * Creates a new instance of this plugin.
-     *
-     * @param pluginInfo This plugin's plugin info
-     * @param identityController Identity Manager
-     * @param controller Parent swing controller
-     * @param actionManager Action manager
-     * @param commandController command controller to register commands
-     */
-    public WindowFlashing(final PluginInfo pluginInfo,
-            final IdentityController identityController,
-            final SwingController controller,
-            final ActionManager actionManager,
-            final CommandController commandController) {
+    public WindowFlashing(final PluginInfo pluginInfo) {
         this.pluginInfo = pluginInfo;
-        this.controller = controller;
-        this.actionManager = actionManager;
-        binder = identityController.getGlobalConfiguration().getBinder();
-        registerCommand(new FlashWindow(this, commandController), FlashWindow.INFO);
     }
 
-    /**
-     * Flashes an inactive window under windows.
-     */
-    public void flashWindow() {
-        if (!mainFrame.isFocused()) {
-            user32.FlashWindowEx(setupFlashObject());
-        }
+    @Override
+    public void load(final PluginInfo pluginInfo, final ObjectGraph graph) {
+        super.load(pluginInfo, graph);
+        this.pluginInfo = pluginInfo;
+
+        setObjectGraph(graph.plus(new WindowFlashingModule()));
+        registerCommand(FlashWindow.class, FlashWindow.INFO);
+        manager = getObjectGraph().get(WindowFlashingManager.class);
     }
 
     /**
@@ -120,31 +66,21 @@ public class WindowFlashing extends BaseCommandPlugin implements ActionListener 
      */
     @Exported
     public void flashNotification(final String title, final String message) {
-        flashWindow();
+        manager.flashWindow();
     }
 
-    /** {@inheritDoc} */
     @Override
     public void onLoad() {
-        mainFrame = controller.getMainFrame();
-        user32 = (User32) Native.loadLibrary("user32", User32.class);
-        binder.bind(this, WindowFlashing.class);
-        actionManager.registerListener(this, CoreActionType.CLIENT_FOCUS_GAINED);
         super.onLoad();
+        manager.onLoad();
     }
 
-    /** {@inheritDoc} */
     @Override
     public void onUnload() {
         super.onUnload();
-        actionManager.unregisterListener(this);
-        binder.unbind(this);
-        mainFrame = null;
-        user32 = null;
-        NativeLibrary.getInstance("user32").dispose();
+        manager.onUnload();
     }
 
-    /** {@inheritDoc} */
     @Override
     public void showConfig(final PreferencesDialogModel manager) {
         final PreferencesCategory category = new PluginPreferencesCategory(
@@ -152,93 +88,25 @@ public class WindowFlashing extends BaseCommandPlugin implements ActionListener 
                 "General configuration for window flashing plugin.");
 
         category.addSetting(new PreferencesSetting(
-                PreferencesType.OPTIONALINTEGER, getDomain(), "blinkrate",
+                PreferencesType.OPTIONALINTEGER, pluginInfo.getDomain(), "blinkrate",
                 "Blink rate", "Specifies the rate at which the taskbar and or "
                 + "caption will blink, if unspecified this will be your cursor "
                 + "blink rate.",
                 manager.getConfigManager(), manager.getIdentity()));
         category.addSetting(new PreferencesSetting(
-                PreferencesType.OPTIONALINTEGER, getDomain(), "flashcount",
+                PreferencesType.OPTIONALINTEGER, pluginInfo.getDomain(), "flashcount",
                 "Flash count", "Specifies the number of times to blink, if "
                 + "unspecified this will blink indefinitely",
                 manager.getConfigManager(), manager.getIdentity()));
         category.addSetting(new PreferencesSetting(PreferencesType.BOOLEAN,
-                getDomain(), "flashtaskbar", "Flash taskbar",
+                pluginInfo.getDomain(), "flashtaskbar", "Flash taskbar",
                 "Should the taskbar entry flash?",
                 manager.getConfigManager(), manager.getIdentity()));
         category.addSetting(new PreferencesSetting(PreferencesType.BOOLEAN,
-                getDomain(), "flashcaption", "Flash caption",
+                pluginInfo.getDomain(), "flashcaption", "Flash caption",
                 "Should the window caption flash?",
                 manager.getConfigManager(), manager.getIdentity()));
 
         manager.getCategory("Plugins").addSubCategory(category);
-    }
-
-    /**
-     * Creates a new flash info object that starts flashing with the configured
-     * settings.
-     */
-    private FLASHWINFO setupFlashObject() {
-        final FLASHWINFO flashInfo = new FLASHWINFO();
-        flashInfo.dwFlags = getFlags();
-        flashInfo.dwTimeout = blinkrate;
-        flashInfo.uCount = flashcount;
-        flashInfo.hWnd = getHWND();
-        flashInfo.cbSize = flashInfo.size();
-        return flashInfo;
-    }
-
-    /**
-     * Creates a new flash object that stops the flashing.
-     */
-    private FLASHWINFO stopFlashObject() {
-        final FLASHWINFO flashInfo = new FLASHWINFO();
-        flashInfo.dwFlags = WinUser.FLASHW_STOP;
-        flashInfo.dwTimeout = 0;
-        flashInfo.uCount = 0;
-        flashInfo.hWnd = getHWND();
-        flashInfo.cbSize = flashInfo.size();
-        return flashInfo;
-    }
-
-    /**
-     * Returns the native handle object for the main frame.
-     * @return
-     */
-    private HWND getHWND() {
-        final HWND hwnd = new HWND();
-        final Pointer pointer = Native.getWindowPointer(mainFrame);
-        hwnd.setPointer(pointer);
-        return hwnd;
-    }
-
-    /**
-     * Calculates the flags for the flash object based on config settings.
-     *
-     * @return Flash info flags
-     */
-    private int getFlags() {
-        int returnValue = 0;
-        if (flashtaskbar) {
-            returnValue |= WinUser.FLASHW_TRAY;
-        }
-        if (flashcaption) {
-            returnValue |= WinUser.FLASHW_CAPTION;
-        }
-        if (flashcount >= 0) {
-            returnValue |= WinUser.FLASHW_TIMER;
-        } else {
-            returnValue |= WinUser.FLASHW_TIMERNOFG;
-        }
-        return returnValue;
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    public void processEvent(final ActionType type, final StringBuffer format,
-            final Object... arguments) {
-        if (mainFrame != null) {
-            user32.FlashWindowEx(stopFlashObject());
-        }
     }
 }
---
+++ src/com/dmdirc/addons/windowflashing/WindowFlashingManager.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright (c) 2006-2014 DMDirc Developers
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc.addons.windowflashing;
+
+import com.dmdirc.ClientModule.GlobalConfig;
+import com.dmdirc.actions.CoreActionType;
+import com.dmdirc.addons.ui_swing.MainFrame;
+import com.dmdirc.config.ConfigBinder;
+import com.dmdirc.config.ConfigBinding;
+import com.dmdirc.interfaces.ActionController;
+import com.dmdirc.interfaces.ActionListener;
+import com.dmdirc.interfaces.actions.ActionType;
+import com.dmdirc.interfaces.config.AggregateConfigProvider;
+
+import javax.inject.Inject;
+
+import com.sun.jna.Native;
+import com.sun.jna.NativeLibrary;
+import com.sun.jna.Pointer;
+import com.sun.jna.platform.win32.User32;
+import com.sun.jna.platform.win32.WinDef;
+import com.sun.jna.platform.win32.WinUser;
+
+public class WindowFlashingManager implements ActionListener {
+
+    /** Swing main frame. */
+    private final MainFrame mainFrame;
+    /** Action controller. */
+    private final ActionController actionController;
+    /** Config binder. */
+    private final ConfigBinder binder;
+    /** Cached blink rate setting. */
+    @ConfigBinding(domain="plugin-windowflashing", key="blinkrate",
+            fallbacks={"plugin-windowflashing", "blinkratefallback"})
+    private int blinkrate;
+    /** Cached count setting. */
+    @ConfigBinding(domain="plugin-windowflashing", key="flashcount",
+            fallbacks={"plugin-windowflashing", "flashcountfallback"})
+    private int flashcount;
+    /** Cached flash taskbar setting. */
+    @ConfigBinding(domain="plugin-windowflashing", key="flashtaskbar")
+    private boolean flashtaskbar;
+    /** Cached flash caption setting. */
+    @ConfigBinding(domain="plugin-windowflashing", key="flashcaption")
+    private boolean flashcaption;
+    /** Library instance. */
+    private User32 user32;
+
+    @Inject
+    public WindowFlashingManager(@GlobalConfig final AggregateConfigProvider config,
+            final MainFrame mainFrame, final ActionController actionController) {
+        this.mainFrame = mainFrame;
+        this.actionController = actionController;
+        binder = config.getBinder();
+    }
+
+    public void onLoad() {
+        user32 = (User32) Native.loadLibrary("user32", User32.class);
+        binder.bind(this, WindowFlashing.class);
+        actionController.registerListener(this, CoreActionType.CLIENT_FOCUS_GAINED);
+    }
+
+    public void onUnload() {
+        actionController.unregisterListener(this);
+        binder.unbind(this);
+        user32 = null;
+        NativeLibrary.getInstance("user32").dispose();
+    }
+
+    /**
+     * Flashes an inactive window under windows.
+     */
+    public void flashWindow() {
+        if (!mainFrame.isFocused()) {
+            user32.FlashWindowEx(setupFlashObject());
+        }
+    }
+
+    /**
+     * Creates a new flash info object that starts flashing with the configured
+     * settings.
+     */
+    private WinUser.FLASHWINFO setupFlashObject() {
+        final WinUser.FLASHWINFO flashInfo = new WinUser.FLASHWINFO();
+        flashInfo.dwFlags = getFlags();
+        flashInfo.dwTimeout = blinkrate;
+        flashInfo.uCount = flashcount;
+        flashInfo.hWnd = getHWND();
+        flashInfo.cbSize = flashInfo.size();
+        return flashInfo;
+    }
+
+    /**
+     * Creates a new flash object that stops the flashing.
+     */
+    private WinUser.FLASHWINFO stopFlashObject() {
+        final WinUser.FLASHWINFO flashInfo = new WinUser.FLASHWINFO();
+        flashInfo.dwFlags = WinUser.FLASHW_STOP;
+        flashInfo.dwTimeout = 0;
+        flashInfo.uCount = 0;
+        flashInfo.hWnd = getHWND();
+        flashInfo.cbSize = flashInfo.size();
+        return flashInfo;
+    }
+
+    /**
+     * Returns the native handle object for the main frame.
+     * @return
+     */
+    private WinDef.HWND getHWND() {
+        final WinDef.HWND hwnd = new WinDef.HWND();
+        final Pointer pointer = Native.getWindowPointer(mainFrame);
+        hwnd.setPointer(pointer);
+        return hwnd;
+    }
+
+    /**
+     * Calculates the flags for the flash object based on config settings.
+     *
+     * @return Flash info flags
+     */
+    private int getFlags() {
+        int returnValue = 0;
+        if (flashtaskbar) {
+            returnValue |= WinUser.FLASHW_TRAY;
+        }
+        if (flashcaption) {
+            returnValue |= WinUser.FLASHW_CAPTION;
+        }
+        if (flashcount >= 0) {
+            returnValue |= WinUser.FLASHW_TIMER;
+        } else {
+            returnValue |= WinUser.FLASHW_TIMERNOFG;
+        }
+        return returnValue;
+    }
+
+    @Override
+    public void processEvent(final ActionType type, final StringBuffer format,
+            final Object... arguments) {
+        if (mainFrame != null) {
+            user32.FlashWindowEx(stopFlashObject());
+        }
+    }
+}
