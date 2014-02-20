c1deed7dd40cbbb670d21ffe5c599b053beb7d3c
Greg Holmes <greg@dmdirc.com>
2014-02-20 19:32:53 +0000

    Adds prefs settings directly from plugin.
    Change-Id: Ib24472d974fe597f3ee564995ef18005eb34136f
    Fixes-Issue: CLIENT-428

--- src/com/dmdirc/addons/windowstatus/WindowStatusPlugin.java
+++ src/com/dmdirc/addons/windowstatus/WindowStatusPlugin.java
@@ -22,7 +22,11 @@
 
 package com.dmdirc.addons.windowstatus;
 
+import com.dmdirc.config.prefs.PluginPreferencesCategory;
+import com.dmdirc.config.prefs.PreferencesCategory;
 import com.dmdirc.config.prefs.PreferencesDialogModel;
+import com.dmdirc.config.prefs.PreferencesSetting;
+import com.dmdirc.config.prefs.PreferencesType;
 import com.dmdirc.plugins.PluginInfo;
 import com.dmdirc.plugins.implementations.BasePlugin;
 
@@ -60,7 +64,23 @@ public class WindowStatusPlugin extends BasePlugin {
 
     @Override
     public void showConfig(final PreferencesDialogModel manager) {
-        windowStatusManager.showConfig(manager, pluginInfo);
+        final PreferencesCategory category = new PluginPreferencesCategory(
+                pluginInfo, "Window status", "");
+
+        category.addSetting(new PreferencesSetting(PreferencesType.BOOLEAN,
+                pluginInfo.getDomain(), "channel.shownone", "Show 'none' count",
+                "Should the count for users with no state be shown?",
+                manager.getConfigManager(), manager.getIdentity()));
+        category.addSetting(new PreferencesSetting(PreferencesType.TEXT,
+                pluginInfo.getDomain(), "channel.noneprefix", "'None' count prefix",
+                "The Prefix to use when showing the 'none' count",
+                manager.getConfigManager(), manager.getIdentity()));
+        category.addSetting(new PreferencesSetting(PreferencesType.BOOLEAN,
+                pluginInfo.getDomain(), "client.showname", "Show real name",
+                "Should the realname for clients be shown if known?",
+                manager.getConfigManager(), manager.getIdentity()));
+
+        manager.getCategory("Plugins").addSubCategory(category);
     }
 
 }
