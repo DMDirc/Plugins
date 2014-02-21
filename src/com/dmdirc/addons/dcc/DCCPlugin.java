a574db388984fb7ed496a57a1fd06a80ea084a53
Greg Holmes <greg@dmdirc.com>
2014-02-21 16:42:04 +0000

    Fix DCC Plugin show config.
    Change-Id: I5802f1deb677a5f9eb84122027314577e902b66e
    Fixes-Issue: CLIENT-440
    Reviewed-on: http://gerrit.dmdirc.com/3156
    Reviewed-by: Chris Smith <chris@dmdirc.com>
    Automatic-Compile: DMDirc Build Manager

--- src/com/dmdirc/addons/dcc/DCCPlugin.java
+++ src/com/dmdirc/addons/dcc/DCCPlugin.java
@@ -22,7 +22,11 @@
 
 package com.dmdirc.addons.dcc;
 
+import com.dmdirc.config.prefs.PluginPreferencesCategory;
+import com.dmdirc.config.prefs.PreferencesCategory;
 import com.dmdirc.config.prefs.PreferencesDialogModel;
+import com.dmdirc.config.prefs.PreferencesSetting;
+import com.dmdirc.config.prefs.PreferencesType;
 import com.dmdirc.plugins.PluginInfo;
 import com.dmdirc.plugins.implementations.BaseCommandPlugin;
 
@@ -33,9 +37,13 @@ import dagger.ObjectGraph;
  */
 public class DCCPlugin extends BaseCommandPlugin {
 
+    private final PluginInfo pluginInfo;
     private DCCManager dccManager;
 
-    /** {@inheritDoc} */
+    public DCCPlugin(final PluginInfo pluginInfo) {
+        this.pluginInfo = pluginInfo;
+    }
+
     @Override
     public void load(final PluginInfo pluginInfo, final ObjectGraph graph) {
         super.load(pluginInfo, graph);
@@ -45,24 +53,80 @@ public class DCCPlugin extends BaseCommandPlugin {
         registerCommand(DCCCommand.class, DCCCommand.INFO);
     }
 
-    /** {@inheritDoc} */
     @Override
     public void onLoad() {
         super.onLoad();
         dccManager.onLoad();
     }
 
-    /** {@inheritDoc} */
     @Override
     public void onUnload() {
         super.onUnload();
         dccManager.onUnload();
     }
 
-    /** {@inheritDoc} */
     @Override
     public void showConfig(final PreferencesDialogModel manager) {
-        dccManager.showConfig(manager);
+                final PreferencesCategory general = new PluginPreferencesCategory(
+                pluginInfo, "DCC", "", "category-dcc");
+        final PreferencesCategory firewall = new PluginPreferencesCategory(
+                pluginInfo, "Firewall", "");
+        final PreferencesCategory sending = new PluginPreferencesCategory(
+                pluginInfo, "Sending", "");
+        final PreferencesCategory receiving = new PluginPreferencesCategory(
+                pluginInfo, "Receiving", "");
+
+        manager.getCategory("Plugins").addSubCategory(general.setInlineAfter());
+        general.addSubCategory(firewall.setInline());
+        general.addSubCategory(sending.setInline());
+        general.addSubCategory(receiving.setInline());
+
+        firewall.addSetting(new PreferencesSetting(PreferencesType.TEXT,
+                pluginInfo.getDomain(), "firewall.ip", "Forced IP",
+                "What IP should be sent as our IP (Blank = work it out)",
+                manager.getConfigManager(), manager.getIdentity()));
+        firewall.addSetting(new PreferencesSetting(PreferencesType.BOOLEAN,
+                pluginInfo.getDomain(), "firewall.ports.usePortRange", "Use Port Range",
+                "Useful if you have a firewall that only forwards specific "
+                + "ports", manager.getConfigManager(), manager.getIdentity()));
+        firewall.addSetting(new PreferencesSetting(PreferencesType.INTEGER,
+                pluginInfo.getDomain(), "firewall.ports.startPort", "Start Port",
+                "Port to try to listen on first", manager.getConfigManager(),
+                manager.getIdentity()));
+        firewall.addSetting(new PreferencesSetting(PreferencesType.INTEGER,
+                pluginInfo.getDomain(), "firewall.ports.endPort", "End Port",
+                "Port to try to listen on last", manager.getConfigManager(),
+                manager.getIdentity()));
+        receiving.addSetting(new PreferencesSetting(PreferencesType.DIRECTORY,
+                pluginInfo.getDomain(), "receive.savelocation", "Default save location",
+                "Where the save as window defaults to?",
+                manager.getConfigManager(), manager.getIdentity()));
+        sending.addSetting(new PreferencesSetting(PreferencesType.BOOLEAN,
+                pluginInfo.getDomain(), "send.reverse", "Reverse DCC",
+                "With reverse DCC, the sender connects rather than "
+                + "listens like normal dcc", manager.getConfigManager(),
+                manager.getIdentity()));
+        sending.addSetting(new PreferencesSetting(PreferencesType.BOOLEAN,
+                pluginInfo.getDomain(), "send.forceturbo", "Use Turbo DCC",
+                "Turbo DCC doesn't wait for ack packets. this is "
+                + "faster but not always supported.",
+                manager.getConfigManager(), manager.getIdentity()));
+        receiving.addSetting(new PreferencesSetting(PreferencesType.BOOLEAN,
+                pluginInfo.getDomain(), "receive.reverse.sendtoken",
+                "Send token in reverse receive",
+                "If you have problems with reverse dcc receive resume,"
+                + " try toggling this.", manager.getConfigManager(),
+                manager.getIdentity()));
+        general.addSetting(new PreferencesSetting(PreferencesType.INTEGER,
+                pluginInfo.getDomain(), "send.blocksize", "Blocksize to use for DCC",
+                "Change the block size for send/receive, this can "
+                + "sometimes speed up transfers.", manager.getConfigManager(),
+                manager.getIdentity()));
+        general.addSetting(new PreferencesSetting(PreferencesType.BOOLEAN,
+                pluginInfo.getDomain(), "general.percentageInTitle",
+                "Show percentage of transfers in the window title",
+                "Show the current percentage of transfers in the DCC window "
+                + "title", manager.getConfigManager(), manager.getIdentity()));
     }
 
 }
