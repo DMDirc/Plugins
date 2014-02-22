a34e8af81d246948238b8d919be74c742448c496
Chris Smith <chris@dmdirc.com>
2014-02-22 11:59:24 +0000

    DI the logging plugin.
    Change-Id: I7b8fd7a83d4431dcf551a171fbb23caeb6100c2e

---
+++ src/com/dmdirc/addons/logging/LoggingModule.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2006-2014 DMDirc Developers
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc.addons.logging;
+
+import com.dmdirc.ClientModule;
+import com.dmdirc.ClientModule.GlobalConfig;
+import com.dmdirc.ClientModule.UserConfig;
+import com.dmdirc.commandline.CommandLineOptionsModule.Directory;
+import com.dmdirc.commandline.CommandLineOptionsModule.DirectoryType;
+import com.dmdirc.interfaces.config.AggregateConfigProvider;
+import com.dmdirc.interfaces.config.ConfigProvider;
+
+import java.io.File;
+
+import javax.inject.Qualifier;
+
+import dagger.Module;
+import dagger.Provides;
+
+/**
+ *
+ * @author chris
+ */
+@Module(addsTo = ClientModule.class, injects = {LoggingManager.class, LoggingCommand.class})
+public class LoggingModule {
+
+    @Qualifier
+    public @interface LoggingDomain {
+    }
+
+    public static final String LOGS_DIRECTORY = "logs";
+
+    private final String domain;
+
+    public LoggingModule(final String domain) {
+        this.domain = domain;
+    }
+
+    @Provides
+    @LoggingDomain
+    public String getDomain() {
+        return domain;
+    }
+
+    @Provides
+    @Directory(LOGS_DIRECTORY)
+    public String getLogsDirectory(
+            @UserConfig final ConfigProvider userConfig,
+            @GlobalConfig final AggregateConfigProvider globalConfig,
+            @Directory(DirectoryType.BASE) final String baseDirectory,
+            @LoggingDomain final String domain) {
+        if (!userConfig.hasOptionString(domain, "general.directory")) {
+            userConfig.setOption(domain, "general.directory",
+                    baseDirectory + "logs" + File.separator);
+        }
+
+        return globalConfig.getOptionString(domain, "general.directory");
+    }
+
+}
