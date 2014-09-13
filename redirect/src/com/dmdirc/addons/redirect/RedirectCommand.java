b6766f236739dc106168ad41f642b98b67d5c237
Chris Smith <chris@dmdirc.com>
2014-09-14 00:42:18 +0100

    Plugin changes for core TabCompletion updates.
    Change-Id: Ibfe55b87e9bde40638f3b169153c653d59c6d6f0
    Depends-On: I80df12d3494a651bc63117d26b1caf94771a51dd

--- redirect/src/com/dmdirc/addons/redirect/RedirectCommand.java
+++ redirect/src/com/dmdirc/addons/redirect/RedirectCommand.java
@@ -34,7 +34,7 @@ import com.dmdirc.commandparser.commands.context.CommandContext;
 import com.dmdirc.interfaces.CommandController;
 import com.dmdirc.messages.MessageSinkManager;
 import com.dmdirc.ui.input.AdditionalTabTargets;
-import com.dmdirc.ui.input.TabCompleter;
+import com.dmdirc.ui.input.TabCompleterUtils;
 import com.dmdirc.util.URLBuilder;
 
 import javax.annotation.Nonnull;
@@ -89,7 +89,7 @@ public class RedirectCommand extends Command implements IntelligentCommand {
     @Override
     public AdditionalTabTargets getSuggestions(final int arg,
             final IntelligentCommandContext context) {
-        return TabCompleter.getIntelligentResults(arg, context, 0);
+        return TabCompleterUtils.getIntelligentResults(arg, context, 0);
     }
 
 }
