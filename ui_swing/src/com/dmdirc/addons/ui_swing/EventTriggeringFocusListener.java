845307fdc8138d8964a3d4bcb28f3605db518fd3
Greg Holmes <greg@dmdirc.com>
2014-12-17 21:09:48 +0000

    Split focus listener into its own class.

---
+++ ui_swing/src/com/dmdirc/addons/ui_swing/EventTriggeringFocusListener.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2006-2014 DMDirc Developers
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.dmdirc.addons.ui_swing;
+
+import com.dmdirc.DMDircMBassador;
+import com.dmdirc.events.ClientFocusGainedEvent;
+import com.dmdirc.events.ClientFocusLostEvent;
+
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
+
+/**
+* Focus listener that triggers focus events on an event bus.
+*/
+class EventTriggeringFocusListener implements WindowFocusListener {
+
+    private final DMDircMBassador eventBus;
+
+    public EventTriggeringFocusListener(final DMDircMBassador eventBus) {
+        this.eventBus = eventBus;
+    }
+
+    @Override
+    public void windowGainedFocus(final WindowEvent e) {
+        eventBus.publishAsync(new ClientFocusGainedEvent());
+    }
+
+    @Override
+    public void windowLostFocus(final WindowEvent e) {
+        eventBus.publishAsync(new ClientFocusLostEvent());
+    }
+}
