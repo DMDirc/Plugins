a8457b6cf7f4e3de229dae305910796f06534f99
Chris Smith <chris@dmdirc.com>
2014-12-27 23:19:52 +0000

    Moved StyledDocument et al into swing.

--- ui_swing/src/com/dmdirc/addons/ui_swing/framemanager/tree/NodeLabel.java
+++ ui_swing/src/com/dmdirc/addons/ui_swing/framemanager/tree/NodeLabel.java
@@ -30,7 +30,7 @@ import com.dmdirc.events.FrameIconChangedEvent;
 import com.dmdirc.events.FrameNameChangedEvent;
 import com.dmdirc.events.NotificationSetEvent;
 import com.dmdirc.ui.IconManager;
-import com.dmdirc.ui.messages.StyledDocumentMaker;
+import com.dmdirc.addons.ui_swing.textpane.StyledDocumentMaker;
 import com.dmdirc.ui.messages.Styliser;
 
 import java.awt.Color;
