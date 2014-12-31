dc25ff963c23935d69034cb110f11f21c83d4173
Greg Holmes <greg@dmdirc.com>
2014-12-31 12:16:19 +0000

    Remove unused import.

--- contactlist/src/com/dmdirc/addons/contactlist/ContactListCommand.java
+++ contactlist/src/com/dmdirc/addons/contactlist/ContactListCommand.java
@@ -31,7 +31,6 @@ import com.dmdirc.commandparser.commands.Command;
 import com.dmdirc.commandparser.commands.IntelligentCommand;
 import com.dmdirc.commandparser.commands.context.ChannelCommandContext;
 import com.dmdirc.commandparser.commands.context.CommandContext;
-import com.dmdirc.events.NickListClientsChangedEvent;
 import com.dmdirc.interfaces.CommandController;
 import com.dmdirc.ui.input.AdditionalTabTargets;
 
