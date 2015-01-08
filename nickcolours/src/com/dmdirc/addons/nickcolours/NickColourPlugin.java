b4efab87d9f65bca9847c291dc507a865174b686
Chris Smith <chris@dmdirc.com>
2015-01-08 22:35:49 +0000

    Make the nick colour plugin use events for prefs.
    DMDirc/DMDirc#428

--- nickcolours/src/com/dmdirc/addons/nickcolours/NickColourPlugin.java
+++ nickcolours/src/com/dmdirc/addons/nickcolours/NickColourPlugin.java
@@ -22,19 +22,8 @@
 
 package com.dmdirc.addons.nickcolours;
 
-import com.dmdirc.addons.ui_swing.SwingController;
-import com.dmdirc.addons.ui_swing.UIUtilities;
-import com.dmdirc.config.prefs.PluginPreferencesCategory;
-import com.dmdirc.config.prefs.PreferencesCategory;
-import com.dmdirc.config.prefs.PreferencesDialogModel;
-import com.dmdirc.config.prefs.PreferencesSetting;
-import com.dmdirc.config.prefs.PreferencesType;
 import com.dmdirc.plugins.PluginInfo;
 import com.dmdirc.plugins.implementations.BasePlugin;
-import com.dmdirc.ui.IconManager;
-import com.dmdirc.ui.messages.ColourManager;
-
-import java.awt.Window;
 
 import dagger.ObjectGraph;
 
@@ -43,25 +32,8 @@ import dagger.ObjectGraph;
  */
 public class NickColourPlugin extends BasePlugin {
 
-    /** Plugin info. */
-    private final PluginInfo pluginInfo;
-    /** Main window that will own any dialogs. */
-    private final Window mainWindow;
-    /** Icon manager. */
-    private final IconManager iconManager;
-    /** Colour manager. */
-    private final ColourManager colourManager;
     /** Nick colour manager. */
     private NickColourManager nickColourManager;
-
-    public NickColourPlugin(final PluginInfo pluginInfo, final SwingController controller,
-            final IconManager iconManager,
-            final ColourManager colourManager) {
-        this.pluginInfo = pluginInfo;
-        this.mainWindow = controller.getMainFrame();
-        this.iconManager = iconManager;
-        this.colourManager = colourManager;
-    }
 
     @Override
     public void load(final PluginInfo pluginInfo, final ObjectGraph graph) {
@@ -69,6 +41,7 @@ public class NickColourPlugin extends BasePlugin {
 
         setObjectGraph(graph.plus(new NickColourModule(pluginInfo.getDomain())));
         nickColourManager = getObjectGraph().get(NickColourManager.class);
+        nickColourManager.setPluginInfo(pluginInfo);
     }
 
     @Override
@@ -81,35 +54,6 @@ public class NickColourPlugin extends BasePlugin {
     public void onUnload() {
         super.onUnload();
         nickColourManager.onUnload();
-    }
-
-    @Override
-    public void showConfig(final PreferencesDialogModel manager) {
-        final PreferencesCategory general = new PluginPreferencesCategory(
-                pluginInfo, "Nick Colours",
-                "General configuration for NickColour plugin.");
-        final PreferencesCategory colours = new PluginPreferencesCategory(
-                pluginInfo, "Colours",
-                "Set colours for specific nicknames.", UIUtilities.invokeAndWait(
-                () -> new NickColourPanel(mainWindow, iconManager, colourManager,
-                        manager.getIdentity(), manager.getConfigManager(),
-                        pluginInfo.getDomain())));
-
-        general.addSetting(new PreferencesSetting(PreferencesType.BOOLEAN,
-                pluginInfo.getDomain(), "userandomcolour", "Use random colour",
-                "Use a pseudo-random colour for each person?",
-                manager.getConfigManager(), manager.getIdentity()));
-        general.addSetting(new PreferencesSetting(PreferencesType.BOOLEAN,
-                pluginInfo.getDomain(), "useowncolour", "Use colour for own nick",
-                "Always use the same colour for our own nickname?",
-                manager.getConfigManager(), manager.getIdentity()));
-        general.addSetting(new PreferencesSetting(PreferencesType.COLOUR, pluginInfo.getDomain(),
-                "owncolour", "Colour to use for own nick",
-                "Colour used for our own nickname, if above setting is "
-                + "enabled.", manager.getConfigManager(), manager.getIdentity()));
-
-        general.addSubCategory(colours);
-        manager.getCategory("Plugins").addSubCategory(general);
     }
 
 }
